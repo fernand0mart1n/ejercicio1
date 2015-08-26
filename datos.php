1eebc70a8984989ac84d81f79d6fd8f00165ebb1
valde1992 <fernando_valde@hotmail.com>
2015-08-26 11:19:26 -0300

    agregado archivo requerido

---
+++ datos.php
@@ -0,0 +1,5 @@
+<?php
+
+var $provincias = array("Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa",
+"La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fé", "Santiago del Estero",
+"Tierra del Fuego", "Tucumán");
