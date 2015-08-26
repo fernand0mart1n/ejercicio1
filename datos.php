db6f123097c39bb4bb8d3d59c81d0024e2772470
valde1992 <fervaldex1992@gmail.com>
2015-08-26 15:51:16 -0300

    izi

--- datos.php
+++ datos.php
@@ -1,5 +1,7 @@
 <?php
 
-var $provincias = array("Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa",
+$provincias = array("Seleccione una provincia", "Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa",
 "La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fé", "Santiago del Estero",
 "Tierra del Fuego", "Tucumán");
+
+$paises = array("Seleccione su país", "Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Perú", "Uruguay", "Venezuela");
