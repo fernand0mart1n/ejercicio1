1cfcae1a4b5bc2c667e392cbaf083603de09e8b8
valde1992 <fernando_valde@hotmail.com>
2015-08-27 09:28:01 -0300

    Algunas mejoras

--- datos.php
+++ datos.php
@@ -1,7 +1,53 @@
 <?php
 
+/*
+
+PARTE 1
+
+Desarrollar un formulario de ingreso de datos para un DNI. 
+- Aplicar todos los conocimientos previos: HTML5, CSS, Bootstrap, jQuery, etc.
+- Validar los datos ingresados DESDE PHP:
+- presencia (campos obligatorios)
+- formato (ej. sólo números)
+- reglas de negocio (ej. la fecha de vencimiento no puede ser menor o igual que la fecha de nacimiento y emisión).
+- Separar la lógica de la parte visual (archivos PHP separados, usar función require)
+- Si el formulario no es válido, redirigir al formulario mostrando el error correspondiente.
+- Si el formulario es válido, mostrar una nueva página con los datos ingresados.
+- Si se intenta "saltear" la validación (ej. acceder a la nueva página directamente) redirigir a la página inicial (función header).
+
+Subir el trabajo a github, enlazar en la entrega el repositorio correspondiente.
+
+PARTE 2
+
+Al formulario anterior, se debe poder acceder a través de un simple login (usuario/clave). Un solo usuario es suficiente.
+La carga de datos hecha en la entrega anterior deberá guardarse en una base de datos sencilla.
+Luego hacer una pantalla más que sea el listado de los datos cargados.
+Extra: agregar un buscador.
+
+*/
+
+$tipos = array("Seleccione una opción", "DNI", "LC", "LE");
+
+$paises = array("Seleccione su país", "Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Perú", "Uruguay", "Venezuela");
+
 $provincias = array("Seleccione una provincia", "Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa",
 "La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fé", "Santiago del Estero",
 "Tierra del Fuego", "Tucumán");
 
-$paises = array("Seleccione su país", "Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Perú", "Uruguay", "Venezuela");
+function texto($texto){
+	if (isset($texto)) {
+		if (is_string($texto) && strlen($texto) >= 100) {
+			return true;
+		} else {
+			$error['texto'] = "Formato de texto inadecuado. Reingrese correctamente.";
+			return false;
+		}
+	}
+}
+
+/*if((document.getElementById('masc').checked) || (document.getElementById('feme').checked)){
+  return true;
+} else {
+  $error['texto'] = "Debe seleccionar una opción.";
+  return false;
+}*/
