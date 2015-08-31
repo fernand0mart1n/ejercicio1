bd44b7a3aa92bb19718d8fd1902f9b45434c73b6
Fernando M. Valdebenito <fernando_valde@hotmail.com>
2015-08-31 05:06:51 -0300

    cambios
    los cambios son buenos

--- ejercicio.php
+++ ejercicio.php
@@ -9,8 +9,8 @@ require('datos.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>.:: Ejercicio ::.</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<script type="text/javascript" src="lib/js/chiches.js"></script>
 	<script type="text/javascript" src="lib/js/jquery-2.1.4.min.js"></script>
 	<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="lib/js/validar.js"></script>
@@ -19,27 +19,65 @@ require('datos.php');
 <body>
 <div class="container">
  <div class="row">
-  <div class="col-md-2">
+  <div class="col-md-1">
   </div>
-  <div class="col-md-8">
-	<H3>Ingrese sus datos:</H3>
-	<form method="post">
-	Apellido(s): 
-	<input type="text" title="Debe ingresar su(s) nombre(s)" name="apellido" placeholder="Ingrese su(s) apellido(s)" required>
-	<br><br>
-	Nombre(s): 
-	<input type="text" title="Debe ingresar su(s) apellido(s)" name="nombre" placeholder="Ingrese su(s) nombre(s)" required>
-	<br><br>
-	Sexo: <br>
-	<input type="radio" name="sex" id="masc" value="male"> Masculino 
-	<input type="radio" name="sex" id="feme" value="female"> Femenino<br><br>
-	Tipo de documento: 
-	<select>
+  <div class="col-lg-10">
+	<H3 align="center">Ingrese sus datos:</H3>	
+	<form class="form-horizontal" method="post" role="form">
+	<div class="form-group">
+    <label for="nombre" class="col-lg-2 control-label">Nombre(s)</label>
+    <div class="col-lg-10">
+      <input type="email" class="form-control" id="nombre" required
+             placeholder="Nombre(s)">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="apellido" class="col-lg-2 control-label">Apellido(s)</label>
+    <div class="col-lg-10">
+      <input type="email" class="form-control" id="apellido" required
+             placeholder="Apellido(s)">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-lg-2 control-label">Sexo</label>
+    <div class="col-lg-10"><br>
+		<input type="radio" name="sex" id="masc" value="male"> Masculino 
+		<input type="radio" name="sex" id="feme" value="female"> Femenino
+    </div>
+  </div>  
+  <div class="form-group">
+    <label class="col-lg-2 control-label">Tipo de documento</label>
+    <div class="col-lg-10"><br>
+		<select>
 		<?php foreach ($tipos as $tipo){?>
 			<option value="<?php echo $tipo;?>"><?php echo $tipo;?></option>
 		<?php };?>
-	</select>
-	<br><br>
+		</select>
+    </div>
+  </div>
+    <div class="form-group">
+    <label for="apellido" class="col-lg-2 control-label">Documento</label>
+    <div class="col-lg-10">
+		<input type="text" class="form-control" title="Debe ingresar su documento" name="documento" id="documento" placeholder="Ingrese su número de documento" required>
+    </div>
+  </div>
+  
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> No cerrar sesión
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-10">
+      <button type="submit" class="btn btn-default">Entrar</button>
+    </div>
+  </div>
+</form>
+	
 	Número de documento: 
 	<input type="text" title="Debe ingresar su documento" name="documento" placeholder="Ingrese su documento" required>
 	<br><br>
@@ -84,7 +122,7 @@ require('datos.php');
 	<hr>
 	</form>
   </div>
-  <div class="col-md-2">
+  <div class="col-md-1">
   </div>
  </div>
 </div>
--- index.php
+++ index.php
@@ -9,8 +9,8 @@ require('datos.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>.:: Ejercicio ::.</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<script type="text/javascript" src="lib/js/chiches.js"></script>
 	<script type="text/javascript" src="lib/js/jquery-2.1.4.min.js"></script>
 	<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="lib/js/validar.js"></script>
@@ -22,9 +22,11 @@ require('datos.php');
   <div class="col-md-2">
   </div>
   <div class="col-md-8">
-	<H3>Bienvenido</H3>
-	<br>
-	<a href="ejercicio.php"><input class="btn btn-md btn-success" type="submit" value="Ingresar"></a>
+	<H3 align="center">Bienvenido al registro de personas</H3>
+	<img src="img/personas.gif" style="width: 100%;max-height: 100%";>
+	<img src="img/dni.jpg" style="width: 100%;max-height: 100%";>
+	<br><br>
+	<a href="ejercicio.php"><input class="btn btn-md btn-success" style="display: block; margin: 0 auto;" type="submit" value="Ingresar"></a>
 	<hr>
 	</form>
   </div>
