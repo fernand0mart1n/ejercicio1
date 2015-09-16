df8d0832564e8bf98a2f766950a4613f6dbcf586
valde1992 <fernando_valde@hotmail.com>
2015-09-16 08:53:41 -0300

    Composer y dependencia fpdf (Free PDF)

--- vista/result2.php
+++ vista/result2.php
@@ -1,2 +1,17 @@
 <?php 
-header('Content-Type: application/pdf; charset=UTF-8'); echo json_encode($_SESSION['seleccion'], true);
+header('Content-Type: application/pdf; charset=UTF-8');
+
+require('../vendor/setasign/fpdf/fpdf.php');
+include "busquedaweb.php";
+
+if (!isset($_SESSION['seleccion'])){
+	$_SESSION['seleccion'] = "Error de datos";
+} else {
+	$var = $_SESSION['seleccion'];
+}
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,$var,'TRB',2,'L' );
+$pdf->Output();
